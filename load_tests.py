b65290a2b0f2324c7c5027554c103771d2029dfe
alfmorais <alfredneto@hotmail.com>
2022-02-15 16:35:43 -0300

    arquivo de teste para standalone app

---
+++ load_tests.py
@@ -0,0 +1,25 @@
+import sys
+from unittest import TestSuite
+from boot_django import boot_django
+
+boot_django()
+
+default_labels = ["tests", ]
+
+
+def get_suite(labels=default_labels):
+    from django.test.runner import DiscoverRunner
+    runner = DiscoverRunner(verbosity=1)
+    failures = runner.run_tests(labels)
+    if failures:
+        sys.exit(failures)
+
+    return TestSuite()
+
+
+if __name__ == "__main__":
+    labels = default_labels
+    if len(sys.argv[1:]) > 0:
+        labels = sys.argv[1:]
+
+    get_suite(labels)
