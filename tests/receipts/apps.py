9149b69a2967142058e354fc564c0aeaae84a411
alfmorais <alfredneto@hotmail.com>
2022-02-15 16:32:08 -0300

    mov arquivos para raiz do projeto

--- tests/receipts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReceiptsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'receipts'
--- tests/receipts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from receipts import views
-
-
-urlpatterns = [
-    path("receipt_json/", views.receipt_json),
-]
